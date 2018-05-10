bcaf5e7a19a68ed6793edd4255e4f79d3406790a
Suen <383366204@qq.com>
2018-05-10 17:19:42 +0800

    order

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Login from '@/views/Login';
 import Product from '@/views/Product'
 import Message from '@/views/Message'
 import Admin from '@/views/Admin'
+import Order from '@/views/Order';
 
 Vue.use(Router)
 
@@ -30,6 +31,11 @@ export default new Router({
       path: '/Admin',
       name: 'Admin',
       component:Admin
+    },
+    {
+      path:'/Order',
+      name:'Order',
+      component:Order
     }
   ]
 })
